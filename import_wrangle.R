b378067eb1f7dcbb13568990d0fe901f4d547617
Katherine M. Prioli <katherine.prioli@rutgers.edu>
2019-10-31 19:52:35 -0400

    Adding code to pull in remaining NHANES dataset

--- import_wrangle.R
+++ import_wrangle.R
@@ -1,6 +1,6 @@
 # Katherine M. Prioli
 # CSC 8515 Final Project
-# Tue Oct 29 09:27:15 2019 ------------------------------
+# Thu Oct 31 19:42:04 2019 ------------------------------
 
 
 #### Loading libraries ----
@@ -21,8 +21,17 @@ ggthemr("fresh")
 
 # NHANES 2015-2016 data (Source:  https://wwwn.cdc.gov/nchs/nhanes/ContinuousNhanes/Default.aspx?BeginYear=2015)
 
-demog_raw <- read.xport("data/DEMO_I.XPT")     # NHANES 2015-2016 demographics data
-diabetes_raw <- read.xport("data/DIQ_I.XPT")   # NHANES 2015-2016 questionnaire data - diabetes items
+dietbehav_raw <- read.xport("data/DBQ_I.XPT")    # Questionnaire data - dietary behavior
+dietintake_raw <- read.xport("DR1TOT_I.XPT")     # Dietary intake data, Day 1
+PHQ9_raw <- read.xport("data/DPQ_I.XPT")         # Questionnaire data - PHQ-9
+bloodpress_raw <- read.xport("data/BPX_I.XPT")   # Examination data - blood pressure
+bodymeas_raw <- read.xport("data/BMX_I.XPT")     # Examination data - body measures
+demog_raw <- read.xport("data/DEMO_I.XPT")       # Demographics data
+diabetes_raw <- read.xport("data/DIQ_I.XPT")     # Questionnaire data - diabetes items
+medical_raw <- read.xport("data/MCQ_I.XPT")      # Questionnaire data - medical items
+physactiv_raw <- read.xport("data/PAQ_I.XPT")    # Questionnaire data - physical activity
+physfxn_raw <- read.xport("data/PFQ_I.XPT")      # Questionnaire data - physical functioning
+
 
 
 #### Wrangling data ----
